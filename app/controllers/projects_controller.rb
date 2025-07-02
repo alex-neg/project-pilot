4cc634e5c95b1cb70b9009663032f3b0388d5910
Alex N <alexneguleanu@gmail.com>
2025-07-02 15:40:10 +0000

    Defined controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,45 @@ class ProjectsController < ApplicationController
   def index
     @projects = Projects.all
   end
+def show
+  @project = Project.find(params[:id])
+end
 
-  def show
-    @projects = Project.find(params[:id])
+def new
+  @project = Project.new
+end
+
+def create
+  @project = Project.new(project_params)
+  if @project.save
+      redirect_to @project, notice: "Project created!"
+  else
+    render :new
   end
+end
+
+def edit
+  @project = Project.find(params[:id])
+end
+
+def update
+  @project = Project.find(params[:id])
+  if @project.update(project.params)
+    redirect_to @project, notice: "Project updated!"
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @project = Project.find(params[:id])
+  @project.destroy
+  redirect_to projects_path, notice: "Project deleted!"
+end
+
+private
+
+def project_params
+  params.require(:project).permit(:name)
+end
 end
