a6fadac64e69f8fa4481ea3c2b8d8c9453862895
Alex N <alexneguleanu@gmail.com>
2025-07-03 19:28:47 +0000

    SitesController actions

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,13 +1,54 @@
 class SitesController < ApplicationController
   def index
+    @project = Project.find(params[:project_id])
+    @sites = @project.sites
   end
 
   def show
+    @project = Project.find(params[:project_id])
+    @sites = @project.sites.find(params[:id])
   end
 
   def new
+    @project = Project.find(params[:project_id])
+    @sites = @project.sites.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @sites = @project.sites.new(site_params)
+    if @site.save
+      redirect_to project_site_path(@project, @site), notice: "Site created!"
+    else
+      render :new
+    end
   end
 
   def edit
+    @project = Project.find(params[:project_id])
+    @sites = @project.site.find(params[:id])
+  end
+
+  def update
+     @project = Project.find(params[:project_id])
+     @site = @project.sites.find(params[:id])
+     if @site.update(site_params)
+      redirect_to project_site_path(@project, @site), notice: "Site updated!"
+     else
+      render :edit
+     end
+  end
+
+  def destroy
+    @project = Project.find(params[:project_id])
+    @site = @project.sites.find(params[:id])
+    @site.destroy
+    redirect_to project_sites_path(@project), notice: "Site deleted!"
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(:name)
   end
 end
