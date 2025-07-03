1e13084b3e0d47f554112035896bc96de7bac5f8
Alexandru Neguleanu <alexneguleanu@gmail.com>
2025-07-03 12:03:56 +0300

    Overwrote newer changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,46 +1,47 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all
+    @projects = Projects.all
   end
-def show
-  @project = Project.find(params[:id])
-end
 
-def new
-  @project = Project.new
-end
-
-def create
-  @project = Project.new(project_params)
-  if @project.save
-      redirect_to projects_path, notice: "Project created!"
-  else
-    render :new
+  def show
+    @projects = Project.find(params[:id])
   end
-end
 
-def edit
-  @project = Project.find(params[:id])
-end
-
-def update
-  @project = Project.find(params[:id])
-  if @project.update(project_params)
-    redirect_to @project, notice: "Project updated!"
-  else
-    render :edit
+  def new
+    @project = Project.new
   end
-end
 
-def destroy
-  @project = Project.find(params[:id])
-  @project.destroy
-  redirect_to projects_path, notice: "Project deleted!"
-end
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project, notice: "Project created!"
+    else
+      render :new
+    end
+  end
 
-private
+  def edit
+    @project = Project.find(params[:id])
+  end
 
-def project_params
-  params.require(:project).permit(:name)
-end
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Project deleted!"
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
