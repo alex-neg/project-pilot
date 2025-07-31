2f85f9d1e7dd59449460407b5a4a99c9a0187746
Alex N <alexneguleanu@gmail.com>
2025-07-31 20:50:58 +0000

    changes

--- app/components/library/button_component.rb
+++ app/components/library/button_component.rb
@@ -1,9 +1,10 @@
 class Library::ButtonComponent < ApplicationComponent
-  def initialize(label:, path:, method: nil, data: {}, variant: nil, style: "")
+  def initialize(label:, path:, method: nil, data: {}, type: nil, variant: nil, style: "")
     @label = label
     @path = path
     @method = method
     @data = data
+    @type = type
     @variant = variant&.to_sym
     @style = style
   end
