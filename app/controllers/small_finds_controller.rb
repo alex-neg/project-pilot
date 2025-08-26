a9de92a3b155b438ff3d0329e04aed811281a9c6
Alexandru Neguleanu <alexneguleanu@gmail.com>
2025-08-26 10:45:38 +0300

    update

--- app/controllers/small_finds_controller.rb
+++ app/controllers/small_finds_controller.rb
@@ -31,7 +31,7 @@ class SmallFindsController < ApplicationController
 
   def update
     @project = Project.find(params[:project_id])
-    @small_find = @project.small_finds.find(small_finds_params)
+    @small_find = @project.small_finds.find(small_find_params)
     if @small_find.update
       redirect_to project_sample_path(@project, @small_find), notice: "Small Find updated!"
     else
