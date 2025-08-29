785e358161e97f0464b318b5e27dba3076b4a6bb
Alex N <alexneguleanu@gmail.com>
2025-08-29 18:32:38 +0000

    Refactoring DetailTemplateComponent under 'Attributes' parameter

--- app/components/templates/detail_template_component.rb
+++ app/components/templates/detail_template_component.rb
@@ -1,21 +1,24 @@
 class Templates::DetailTemplateComponent < ApplicationComponent
-  def initialize(model:, breadcrumb:, nav_buttons: [], action_buttons: [], show_picture: false, parent: nil)
+  # removing nav_buttons: [], action_buttons: []
+  def initialize(model:, breadcrumb:, attributes: [], show_picture: false, parent: nil)
     @model = model
     @breadcrumb = breadcrumb
-    @nav_buttons = nav_buttons
-    @action_buttons = action_buttons
+    @attributes = attributes
+    # @nav_buttons = nav_buttons
+    # @action_buttons = action_buttons
     @show_picture = show_picture
     @parent = parent
   end
 
-  def model_details
-    {
-      name: @model.name,
-      id: @model.id,
-      description: @model.try(:description),
-      type: (@model.try(:sample_type) || @model.try(:find_type))&.capitalize
-    }
-  end
 
-  attr_reader :model, :breadcrumb, :nav_buttons, :action_buttons, :show_picture
+  # def model_details
+  #   {
+  #     name: @model.name,
+  #     id: @model.id,
+  #     description: @model.try(:description),
+  #     type: (@model.try(:sample_type) || @model.try(:find_type))&.capitalize
+  #   }
+  # end
+
+  attr_reader :model, :breadcrumb, :attributes, :show_picture
 end
