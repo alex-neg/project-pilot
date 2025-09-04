592ffc08ef2c92fc1b2ab42e80da15067c764a13
Alex N <alexneguleanu@gmail.com>
2025-09-04 12:36:06 +0000

    Refactoring based on attributes parameter. Everything working.

--- app/components/library/button_component.rb
+++ app/components/library/button_component.rb
@@ -27,7 +27,7 @@ class Library::ButtonComponent < ApplicationComponent
     {
       primary: "px-4 py-2 font-semibold text-center text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition min-w-[130px]",
       secondary: "px-4 py-2 font-semibold text-center text-indigo-600 border border-indigo-600 rounded-lg hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition min-w-[100px]",
-      danger: "px-4 py-2 font-semibold text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition min-w-[250px]",
+      danger: "px-4 py-2 font-semibold text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition min-w-[100px]",
       link: "text-indigo-600 hover:underline focus:outline-none focus:ring-2 focus:ring-indigo-500 transition",
       link_danger: "text-red-600 hover:underline focus:outline-none focus:ring-2 focus:ring-red-500 transition"
     }
