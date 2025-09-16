520ebf3c35780e4e2433c80a962d7efe68fc2e09
Alex N <alexneguleanu@gmail.com>
2025-09-16 18:48:36 +0000

    Removed show_picture

--- app/components/templates/detail_template_component.rb
+++ app/components/templates/detail_template_component.rb
@@ -1,12 +1,11 @@
 class Templates::DetailTemplateComponent < ApplicationComponent
-  def initialize(model:, items:, attributes: [], actions: [], show_picture: false, parent: nil)
+  def initialize(model:, items:, attributes: [], actions: [], parent: nil)
     @model = model
     @items = items
     @attributes = attributes
     @actions = actions
-    @show_picture = show_picture
     @parent = parent
   end
 
-  attr_reader :model, :items, :attributes, :actions, :show_picture
+  attr_reader :model, :items, :attributes, :actions
 end
