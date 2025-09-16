55a955ea26359446f3b5c39a9f8248a0b1764a9f
Alex N <alexneguleanu@gmail.com>
2025-09-16 14:08:02 +0000

    Addressed Button Component feedback.

--- app/components/library/button_component.rb
+++ app/components/library/button_component.rb
@@ -1,29 +1,23 @@
 class Library::ButtonComponent < ApplicationComponent
-  def initialize(label:, path:, method: nil, data: {}, type: nil, variant: nil, style: "")
+  def initialize(label:, path:, method: nil, data: {}, variant: :primary)
     @label = label
-     @path = path
-    # @method = method
+    @path = path
+    @method = method
     @data = data
-    # @type = type
-    @variant = variant&.to_sym
-    @style = style
+    @variant = variant
   end
 
-  def style
-    @style.presence || default_style
+  def classes
+    return base_classes if @variant.nil?
+
+    variant_classes[@variant] || base_classes
   end
 
-  def default_style
-    base = "text-gray-800 hover:underline cursor-pointer mr-2"
-
-    if @variant.nil?
-      base
-    else
-      "#{variant_styles[@variant] || variant_styles[:primary]}"
-    end
+  def base_classes
+    "text-gray-800 hover:underline cursor-pointer mr-2"
   end
 
-  def variant_styles
+  def variant_classes
     {
       primary: "px-4 py-2 font-semibold text-center text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition min-w-[130px]",
       secondary: "px-4 py-2 font-semibold text-center text-indigo-600 border border-indigo-600 rounded-lg hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition min-w-[100px]",
