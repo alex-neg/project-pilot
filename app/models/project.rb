496bb211eda4791566db83a5f74a2deb1cb280b2
alex-neg <61922442+alex-neg@users.noreply.github.com>
2025-10-31 13:48:19 +0000

    Destroyed controllers and models. Dropped the db.

--- app/models/project.rb
+++
@@ -1,6 +0,0 @@
-class Project < ApplicationRecord
-  validates :name, presence: { message: "Name is required" }
-  has_many :sites, dependent: :destroy
-  has_many :samples, dependent: :destroy
-  has_many :small_finds, dependent: :destroy
-end
--- app/models/site.rb
+++
@@ -1,4 +0,0 @@
-class Site < ApplicationRecord
-  validates :name, presence: { message: "Name is required" }
-  belongs_to :project
-end
