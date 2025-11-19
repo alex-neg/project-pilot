5c1dfa9bc41d21e07f58c0249784c06c8d2df3c5
alex-neg <61922442+alex-neg@users.noreply.github.com>
2025-11-19 21:06:32 +0000

    WIP

--- app/models/context.rb
+++ app/models/context.rb
@@ -9,10 +9,10 @@ class Context < ApplicationRecord
   has_one_attached :plan_image
 
   enum :context_type, { natural: 0, artificial: 1, hybrid: 2 }
-  enum :brightness, { dark: 0, pale: 1, light: 2 }, prefix: true
-  enum :hue, { red: 0, yellow: 1, blue: 2 }, prefix: true
-  enum :colour, { red: 0, green: 1, blue: 2 }
-  enum :compaction, { loose: 0, medium: 1, dense: 2 }, prefix: true
+  enum :brightness, { dark: 0, pale: 1, light: 2 }
+  enum :hue, { red: 0, yellow: 1, blue: 2 }
+  enum :colour, { redish: 0, green: 1, blueish: 2 }
+  enum :compaction, { loose: 0, medium: 1, dense: 2 }
 
   def recorded_by_name
     "#{recorded_by&.first_name} #{recorded_by&.last_name}"
